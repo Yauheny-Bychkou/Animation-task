7bd5e6c6735ee71c07aa63cae0d9b1f9725fb7b5
Yauheny Bychkou <375445433672@yandex.by>
2021-08-21 14:44:12 +0300

    modernization

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
 "use strict";
-/*
-Введённый в поле текст должен отображаться внутри параграфа, но с задержкой в 300 мс.
-При этом каждый введённый пользователем в поле символ сбрасывает предыдущий отложенный вызов и запускает новый.
-Таким образом программа должна ожидать завершения ввода пользователя и только после этого изменять текст в <p></p>
-*/
+
 let input = document.querySelector(".input"),
-  text = document.querySelector("p");
-
-let wormDown = function () {
-  let eventFunc = function (event) {
-    text.textContent = input.value;
-  };
-
-  input.addEventListener("input", eventFunc);
-};
-
-let idInterval = setInterval(wormDown, 300);
-
-/*
-Написать любую анимацию, используя requestAnimationFrame и кнопку, активирующую её
-Кнопка должна ставить анимацию на паузу и продолжать анимацию после повторного нажатия 
-+ Добавить кнопку reset, которая будет возвращать анимацию в первоначальное состояние 
-*/
-
-let worm = document.querySelector(".worm"),
+  text = document.querySelector("p"),
+  worm = document.querySelector(".worm"),
   airplane = document.querySelector(".airplane"),
   buttonStart = document.querySelector(".button-start"),
   buttonReset = document.querySelector(".button-reset"),
+  flyInterval,
+  flyAnimate,
+  wormDown,
+  idInterval,
+  animate,
   count = 0;
 
-let flyInterval;
-let flyAnimate = function () {
+flyAnimate = function () {
   flyInterval = requestAnimationFrame(flyAnimate);
   count++;
   if (count < 450) {
@@ -40,7 +23,8 @@ let flyAnimate = function () {
     cancelAnimationFrame(flyInterval);
   }
 };
-let animate = false;
+
+animate = true;
 buttonStart.addEventListener("click", function () {
   if (animate) {
     flyInterval = requestAnimationFrame(flyAnimate);
