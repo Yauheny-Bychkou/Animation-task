2d78262ba3e12d8369971e9e411a1e05885e6435
Yauheny Bychkou <375445433672@yandex.by>
2021-08-22 11:36:53 +0300

    savemode

--- script.js
+++ script.js
@@ -23,7 +23,39 @@ flyAnimate = function () {
     cancelAnimationFrame(flyInterval);
   }
 };
+function debounce(func, wait, immediate) {
+  let timeout;
 
+  return function executedFunction() {
+    const context = this;
+    const args = arguments;
+
+    const later = function () {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+
+    const callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+}
+
+const returnedFunction = debounce(function (e) {
+  let target = e.target;
+  text.textContent = target.value;
+}, 300);
+
+input.addEventListener("input", returnedFunction);
+window.addEventListener("resize", returnedFunction);
 animate = true;
 buttonStart.addEventListener("click", function () {
   if (animate) {
